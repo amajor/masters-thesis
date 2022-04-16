24ff198bcccfc0394c95ff013c757ad2e74b7f06
Alison Major <alison.major@gmail.com>
2022-04-16 14:14:39 -0400

    Create a summary of the data from Radon and Pylint on a repository level

---
+++ Supporting Data/SQL/2_sum_repository_details.sql
@@ -0,0 +1,35 @@
+-- https://radon.readthedocs.io/en/latest/commandline.html#the-raw-command
+-- The equation sloc+multi+singlecomments+blank=loc should always hold.
+
+-- https://radon.readthedocs.io/en/latest/commandline.html#the-raw-command
+-- The equation sloc+multi+singlecomments+blank=loc should always hold.
+
+SELECT
+	t1_radon_raw.name AS "Repository",
+    TRUNC(AVG(t2_radon_mi.mi::numeric), 2) AS "Average Project MI",
+	SUM(t1_radon_raw.sloc::numeric) AS "Total Project SLOC",
+	SUM(t1_radon_raw.comments::numeric) AS "Total Project Comments",
+    CONCAT(TRUNC(
+      SUM(t1_radon_raw.comments::numeric) / SUM(t1_radon_raw.sloc::numeric) * 100, 
+      2  -- Number of decimal places
+    ), '%') AS "Total Project Comment-to-SLOC Ratio",
+    COUNT(DISTINCT t1_radon_raw.module_name) AS "Total Modules in Project",
+    SUM(
+      CASE
+        WHEN t3_python_metrics.code = 'R' THEN n::numeric
+        ELSE 0
+      END
+    ) AS "Total Refactor Msgs",
+    SUM(
+      CASE
+        WHEN t3_python_metrics.code = 'C' THEN n::numeric
+        ELSE 0
+      END
+    ) AS "Total Convention Msgs"
+FROM new_radon_raw_metrics AS t1_radon_raw
+INNER JOIN new_radon_mi_metric AS t2_radon_mi
+	ON t1_radon_raw.module_name = t2_radon_mi.module_name
+    INNER JOIN new_pylint_metrics_by_msg AS t3_python_metrics
+    	ON t2_radon_mi.module_name = t3_python_metrics.module_name
+GROUP BY t1_radon_raw.name
+ORDER BY t1_radon_raw.name ASC;
