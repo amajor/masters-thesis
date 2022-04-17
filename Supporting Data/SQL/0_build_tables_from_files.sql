8849deef10a39f6c814fe99434355bbee9ad06e0
Alison Major <alison.major@gmail.com>
2022-04-17 19:37:49 -0400

    Group in 4th table: original repository details

--- Supporting Data/SQL/0_build_tables_from_files.sql
+++ Supporting Data/SQL/0_build_tables_from_files.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE new_pylint_metrics (
   name VARCHAR ( 50 ),
   module_name VARCHAR ( 500 ),
@@ -69,93 +70,31 @@ FROM '/Users/alisonmajor/Developer/masters-thesis/Supporting Data/data-exports/2
 DELIMITER ',' 
 CSV HEADER;
 
--- Then we can create views from these data dumps.
--- This allows us to compare our data to see any misalignments.
+CREATE TABLE new_repo_details (
+  repo_full_name VARCHAR ( 250 ),
+  description VARCHAR ( 500 ),
+  url VARCHAR ( 500 ),
+  stars_count INT,
+  watchers_count INT,
+  size_count INT,
+  topics VARCHAR (350),
+  commit_count INT,
+  contributer_count INT,
+  languages_list VARCHAR (9000),
+  created_date TIMESTAMP,
+  last_push_date TIMESTAMP,
+  open_issues_count INT,
+  release_count INT,
+  latest_tag VARCHAR (50),
+  latest_commit_sha VARCHAR (50),
+  license VARCHAR (250),
+  repo_name_2 VARCHAR (50),
+  raw VARCHAR (9000),
+  unknown_1 DECIMAL,
+  unknown_2 INT
+);
 
--- Total refactor and convention messages from Pylint.
-/*
-SELECT
-	name,
-    SUM(n_refactor) AS total_refactor,
-    SUM(n_convention) AS total_convention
-FROM new_pylint_metrics
-GROUP BY name
-ORDER BY name ASC;
-*/
-
-CREATE VIEW new_view_pylint_metrics_totals AS
-SELECT
-	name,
-    SUM(n_refactor) AS total_refactor,
-    SUM(n_convention) AS total_convention
-FROM new_pylint_metrics
-GROUP BY name;
-
--- Total refactor and convention messages from Pylint Msg List.
-/*
-SELECT
-	name,
-    SUM(
-      CASE
-        WHEN code = 'R' THEN n::numeric
-        ELSE 0
-      END
-    ) AS total_refactor,
-    SUM(
-      CASE
-        WHEN code = 'C' THEN n::numeric
-        ELSE 0
-      END
-    ) AS total_convention
-FROM new_pylint_metrics_by_msg
-GROUP BY name
-ORDER BY name ASC;
-*/
-
-CREATE VIEW new_view_pylint_metrics_by_msg_totals AS
-SELECT
-	name,
-    SUM(
-      CASE
-        WHEN code = 'R' THEN n::numeric
-        ELSE 0
-      END
-    ) AS total_refactor,
-    SUM(
-      CASE
-        WHEN code = 'C' THEN n::numeric
-        ELSE 0
-      END
-    ) AS total_convention
-FROM new_pylint_metrics_by_msg
-GROUP BY name;
-
-CREATE VIEW new_view_compare_pylint_metrics_for_validation AS
-SELECT
-  v1.name,
-  CASE
-    WHEN v1.total_refactor = v2.total_refactor
-      THEN 'TRUE'
-    ELSE 'FALSE'
-  END AS total_refactor_match,
-  CASE
-    WHEN v1.total_convention = v2.total_convention
-      THEN 'TRUE'
-    ELSE 'FALSE'
-  END AS total_convention_match,
-  v1.total_refactor AS v1_total_refactor,
-  v2.total_refactor AS v2_total_refactor,
-  v1.total_convention AS v1_total_convention,
-  v2.total_convention AS v2_total_convention
-FROM new_view_pylint_metrics_totals AS v1
-INNER JOIN new_view_pylint_metrics_by_msg_totals AS v2
-  ON v1.name = v2.name;
-
--- CONFIRM ALL MATCHING VALUES
--- If both the pylint_metrics and pylint_metrics_by_msg tables have the same
--- number of messages for Refactor and Convention, then this query below
--- should return 0 records.
-SELECT *
-FROM new_view_compare_pylint_metrics_for_validation
-WHERE total_refactor_match = 'FALSE'
-  OR total_convention_match = 'FALSE';
+COPY new_repo_details
+FROM '/Users/alisonmajor/Developer/masters-thesis/Supporting Data/code-metrics/data/master_repo_133.csv'  
+DELIMITER ',' 
+CSV HEADER;
--- Supporting Data/SQL/2_sum_repository_details.sql
+++ Supporting Data/SQL/2_sum_repository_details.sql
@@ -1,9 +1,6 @@
 -- https://radon.readthedocs.io/en/latest/commandline.html#the-raw-command
 -- The equation sloc+multi+singlecomments+blank=loc should always hold.
 
--- https://radon.readthedocs.io/en/latest/commandline.html#the-raw-command
--- The equation sloc+multi+singlecomments+blank=loc should always hold.
-
 SELECT
 	t1_radon_raw.name AS "Repository",
     TRUNC(AVG(t2_radon_mi.mi::numeric), 2) AS "Average Project MI",
@@ -25,11 +22,16 @@ SELECT
         WHEN t3_python_metrics.code = 'C' THEN n::numeric
         ELSE 0
       END
-    ) AS "Total Convention Msgs"
+    ) AS "Total Convention Msgs",
+    MAX(t4_repo_details.commit_count) AS "Repo Commit Count",
+    MAX(t4_repo_details.contributer_count) AS "Repo Contributer Count",
+    MAX(t4_repo_details.release_count) AS "Repo Release Count"
 FROM new_radon_raw_metrics AS t1_radon_raw
 INNER JOIN new_radon_mi_metric AS t2_radon_mi
 	ON t1_radon_raw.module_name = t2_radon_mi.module_name
     INNER JOIN new_pylint_metrics_by_msg AS t3_python_metrics
-    	ON t2_radon_mi.module_name = t3_python_metrics.module_name
+    	ON t1_radon_raw.module_name = t3_python_metrics.module_name
+        INNER JOIN new_repo_details AS t4_repo_details
+        	ON t1_radon_raw.name = t4_repo_details.repo_name_2
 GROUP BY t1_radon_raw.name
 ORDER BY t1_radon_raw.name ASC;
